7dd9e01ee7a2fa81f3af90aff2da54b7a6d4eede
Darlan Mendonça <darlanmendonca@gmail.com>
2017-01-16 09:30:52 -0200

    begin draft for menu and dialog

--- sources/scripts/mn-autocomplete.class.js
+++ sources/scripts/mn-autocomplete.class.js
@@ -1,6 +1,10 @@
 class MnAutocomplete extends MnInput { // eslint-disable-line no-undef
   constructor(self) {
     self = super(self)
+    // const dialog = document.createElement('mn-dialog')
+    // dialog.id = 'test'
+    // console.log(dialog)
+    // document.body.appendChild(dialog)
     return self
   }
 }
