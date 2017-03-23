340a1e4b9a927d5179aee88f4b3221e4fb1b97d4
Darlan Mendonça <darlanmendonca@gmail.com>
2017-03-23 15:22:23 -0300

    final solution to scroll safari mobile

--- sources/scripts/mn-autocomplete.class.js
+++ sources/scripts/mn-autocomplete.class.js
@@ -5,8 +5,23 @@ class MnAutocomplete extends window.MnInput {
     this.input = this.querySelector('input')
 
     // this.setMobileDialog()
+    this.setOnFocus()
 
     return self
+  }
+
+  setOnFocus() {
+    const mobileDevice = screen.width < 768
+    if (mobileDevice) {
+      this.input.addEventListener('focus', () => {
+        const offsetTop = this.input.getBoundingClientRect().top - 20
+          + window.pageYOffset
+          - document.documentElement.clientTop
+
+        window.scrollTo(0, offsetTop)
+        document.body.scrollTop = offsetTop
+      })
+    }
   }
 
   // setMobileDialog() {
