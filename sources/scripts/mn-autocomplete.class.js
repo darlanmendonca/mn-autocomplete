99c7aa3baf0fbfcb8f01bf760dde373fa7321fe4
Darlan Mendonça <darlanmendonca@gmail.com>
2017-03-24 14:25:07 -0300

    implement a filter

--- sources/scripts/mn-autocomplete.class.js
+++ sources/scripts/mn-autocomplete.class.js
@@ -5,20 +5,48 @@ class MnAutocomplete extends window.MnChips {
     this.input = this.querySelector('input')
     this.menu = this.querySelector('menu')
 
+    this.setFilter()
     this.setOnFocus()
     this.setValidate()
 
     return self
   }
 
+  setFilter() {
+    this.input.addEventListener('keyup', () => {
+      const value = this.input.value
+      const options = Array.from(this.menu.querySelectorAll('.mn-option'))
+      if (value) {
+        options.forEach(option => {
+          const matchOption = testRegex(value, option.textContent)
+          matchOption
+            ? option.classList.remove('hidden')
+            : option.classList.add('hidden')
+        })
+      } else {
+        this.menu.querySelectorAll('.mn-option.hidden').forEach(option => option.classList.remove('hidden'))
+      }
+    })
+
+    this.input.addEventListener('blur', () => {
+      this.menu
+        .querySelectorAll('.mn-option.hidden')
+        .forEach(option => option.classList.remove('hidden'))
+
+      this.menu.scrollTop = 0
+    })
+
+    function testRegex(search, value) {
+      const reg = new RegExp(search.split('').join('.*'), 'i')
+      return reg.test(value)
+    }
+  }
+
   setValidate() {
     this.validateChip = validateChip
-    const menu = this.menu
 
-    function validateChip(value) {
-      console.log(value)
-      console.log(menu)
-      return true
+    function validateChip() {
+      return false
     }
   }
 
